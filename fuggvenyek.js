18a32e3bd5a9326adb132ac58c498abdbee13b77
baalintka <ballint3@gmail.com>
2024-03-13 16:55:51 +0100

    nyerés kezdése

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -2,7 +2,7 @@ export function jatekter() {
   let txt = "";
 
   for (let index = 0; index < 9; index++) {
-    txt += `<button id="but${index}"></button>`;
+    txt += `<button id="${index}"></button>`;
   }
   return txt;
 }
@@ -10,5 +10,11 @@ export function torles(lista){
   for (let index = 0; index < lista.length; index++) {
       lista[index].classList.remove("kor")
       lista[index].classList.remove("x")
+      
   }
+}
+export function nyeres(){
+  let txt = "";
+  txt+="<h1>Nyertél!</h1>"
+  return txt;
 }
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-import{ jatekter, torles} from './fuggvenyek.js'
+import{ jatekter, nyeres, torles} from './fuggvenyek.js'
 const JATEKTERELEM=document.querySelector(".mezo")
+const NYERESELEM=document.querySelector('#nyeres')
 
 JATEKTERELEM.innerHTML=jatekter();
-
+let gyujto=""
 let valtozo=0
 const GOMBOK = document.querySelectorAll(".mezo button");
 for (let index = 0; index < GOMBOK.length; index++) {
@@ -11,15 +12,22 @@ for (let index = 0; index < GOMBOK.length; index++) {
     let aktualisKlikk = event.target;
     if (valtozo%2===0) {
         aktualisKlikk.classList.add("kor");
+        
     }else{
         aktualisKlikk.classList.add("x");
+        console.log(aktualisKlikk.id)
+        gyujto+=aktualisKlikk.id
+        console.log(gyujto)
+        if (gyujto==="012" || gyujto==="102"||gyujto==="201"|| gyujto==="120" ||gyujto==="021" ) {
+          NYERESELEM.innerHTML=nyeres();
+          NYERESELEM.classList.add("nyeresClass")
+        }
     }
     
   });
 }
 const TORLESGOMB=document.querySelector("#torles")
 TORLESGOMB.addEventListener('click',function(){
-
+    gyujto=""
     torles(GOMBOK)
 })
-console.log(GOMBOK[2])
