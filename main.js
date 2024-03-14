e68ee922d5b51bae2e8107352b89ff0f9416b998
szbalint98 <ballint3@gmail.com>
2024-03-14 09:21:34 +0100

    metodus szervezés

--- main.js
+++ main.js
@@ -5,151 +5,59 @@ const JATEKOSELEM=document.querySelector('#jatekos')
 
 JATEKTERELEM.innerHTML=jatekter();
 let valtozo=0
+
 const JELTOMBX=[0,0,0,0,0,0,0,0,0]
 const JELTOMBO=[0,0,0,0,0,0,0,0,0]
+
 JATEKOSELEM.innerHTML+="<h3>1.Játékos köre!</h3>"
-
-
 const GOMBOK = document.querySelectorAll(".mezo button");
 for (let index = 0; index < GOMBOK.length; index++) {
   GOMBOK[index].addEventListener("click", function (event) {
     valtozo++
     let aktualisKlikk = event.target;
     if (valtozo%2===0) {
-      aktualisKlikk.classList.add("kor");
-      JATEKOSELEM.innerHTML=jatekos1()
-      console.log(aktualisKlikk.id)
-      JELTOMBO[aktualisKlikk.id]+=1
-      if (JELTOMBO[0] ===1 && JELTOMBO[1]===1 && JELTOMBO[2]===1) {
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[0].classList.add("rkor");
-        GOMBOK[1].classList.add("rkor");
-        GOMBOK[2].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }else if(JELTOMBO[0] ===1 && JELTOMBO[3]===1 && JELTOMBO[6]===1){
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[0].classList.add("rkor");
-        GOMBOK[3].classList.add("rkor");
-        GOMBOK[6].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }else if(JELTOMBO[0] ===1 && JELTOMBO[4]===1 && JELTOMBO[8]===1){
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[0].classList.add("rkor");
-        GOMBOK[4].classList.add("rkor");
-        GOMBOK[8].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }else if(JELTOMBO[3] ===1 && JELTOMBO[4]===1 && JELTOMBO[5]===1){
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[3].classList.add("rkor");
-        GOMBOK[4].classList.add("rkor");
-        GOMBOK[5].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }else if(JELTOMBO[1] ===1 && JELTOMBO[4]===1 && JELTOMBO[7]===1){
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[1].classList.add("rkor");
-        GOMBOK[4].classList.add("rkor");
-        GOMBOK[7].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }
-      else if(JELTOMBO[3] ===1 && JELTOMBO[6]===1 && JELTOMBO[8]===1){
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[3].classList.add("rkor");
-        GOMBOK[6].classList.add("rkor");
-        GOMBOK[8].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }
-      else if(JELTOMBO[6] ===1 && JELTOMBO[7]===1 && JELTOMBO[8]===1){
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[6].classList.add("rkor");
-        GOMBOK[7].classList.add("rkor");
-        GOMBOK[8].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }
-      else if(JELTOMBO[2] ===1 && JELTOMBO[4]===1 && JELTOMBO[6]===1){
-        NYERESELEM.innerHTML=nyeres();
-        NYERESELEM.classList.add("nyeresClass")
-        GOMBOK[2].classList.add("rkor");
-        GOMBOK[4].classList.add("rkor");
-        GOMBOK[6].classList.add("rkor");
-        JATEKOSELEM.innerHTML=""
-      }
-        
+        aktualisKlikk.classList.add("kor");
+        JATEKOSELEM.innerHTML=jatekos1()
+        console.log(aktualisKlikk.id)
+        JELTOMBO[aktualisKlikk.id]+=1
+        nyertE(0,1,2,JELTOMBO,GOMBOK,"rkor")
+        nyertE(0,3,6,JELTOMBO,GOMBOK,"rkor")
+        nyertE(0,4,8,JELTOMBO,GOMBOK,"rkor")
+        nyertE(3,4,5,JELTOMBO,GOMBOK,"rkor")
+        nyertE(1,4,7,JELTOMBO,GOMBOK,"rkor")
+        nyertE(3,6,8,JELTOMBO,GOMBOK,"rkor")
+        nyertE(6,7,8,JELTOMBO,GOMBOK,"rkor")
+        nyertE(2,4,6,JELTOMBO,GOMBOK,"rkor")
+      
     }else{
         aktualisKlikk.classList.add("x");
         JATEKOSELEM.innerHTML=jatekos2()
         console.log(aktualisKlikk.id)
         JELTOMBX[aktualisKlikk.id]+=1
-        if (JELTOMBX[0] ===1 && JELTOMBX[1]===1 && JELTOMBX[2]===1) {
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[0].classList.add("rx");
-          GOMBOK[1].classList.add("rx");
-          GOMBOK[2].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }else if(JELTOMBX[0] ===1 && JELTOMBX[3]===1 && JELTOMBX[6]===1){
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[0].classList.add("rx");
-          GOMBOK[3].classList.add("rx");
-          GOMBOK[6].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }else if(JELTOMBX[0] ===1 && JELTOMBX[4]===1 && JELTOMBX[8]===1){
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[0].classList.add("rx");
-          GOMBOK[4].classList.add("rx");
-          GOMBOK[8].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }else if(JELTOMBX[3] ===1 && JELTOMBX[4]===1 && JELTOMBX[5]===1){
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[3].classList.add("rx");
-          GOMBOK[4].classList.add("rx");
-          GOMBOK[5].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }else if(JELTOMBX[1] ===1 && JELTOMBX[4]===1 && JELTOMBX[7]===1){
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[1].classList.add("rx");
-          GOMBOK[4].classList.add("rx");
-          GOMBOK[7].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }
-        else if(JELTOMBX[3] ===1 && JELTOMBX[6]===1 && JELTOMBX[8]===1){
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[3].classList.add("rx");
-          GOMBOK[6].classList.add("rx");
-          GOMBOK[8].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }
-        else if(JELTOMBX[6] ===1 && JELTOMBX[7]===1 && JELTOMBX[8]===1){
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[6].classList.add("rx");
-          GOMBOK[7].classList.add("rx");
-          GOMBOK[8].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }else if(JELTOMBX[2] ===1 && JELTOMBX[4]===1 && JELTOMBX[6]===1){
-          NYERESELEM.innerHTML=nyeres();
-          NYERESELEM.classList.add("nyeresClass")
-          GOMBOK[2].classList.add("rx");
-          GOMBOK[4].classList.add("rx");
-          GOMBOK[6].classList.add("rx");
-          JATEKOSELEM.innerHTML=""
-        }
+        nyertE(0,1,2,JELTOMBX,GOMBOK,"rx")
+        nyertE(0,3,6,JELTOMBX,GOMBOK,"rx")
+        nyertE(0,4,8,JELTOMBX,GOMBOK,"rx")
+        nyertE(3,4,5,JELTOMBX,GOMBOK,"rx")
+        nyertE(1,4,7,JELTOMBX,GOMBOK,"rx")
+        nyertE(3,6,8,JELTOMBX,GOMBOK,"rx")
+        nyertE(6,7,8,JELTOMBX,GOMBOK,"rx")
+        nyertE(2,4,6,JELTOMBX,GOMBOK,"rx")
+      
     }
-    
-  });
-}
+    }
+    )
+  };
+
 const TORLESGOMB=document.querySelector("#torles")
 TORLESGOMB.addEventListener('click',torles,{
   
 })
+function nyertE(a,b,c,tomb,gomb,css){
+  if (tomb[a] ===1 && tomb[b]===1 && tomb[c]===1) {
+    NYERESELEM.innerHTML=nyeres();
+    NYERESELEM.classList.add("nyeresClass")
+    gomb[a].classList.add(css);
+    gomb[b].classList.add(css);
+    gomb[c].classList.add(css);
+    JATEKOSELEM.innerHTML=""
+}}
